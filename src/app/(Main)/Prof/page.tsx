7e6d9a560b5105338dfb4545c32288e7fab368e6
Ryan <ryanlai2486@gmail.com>
2025-02-07 16:14:35 +0300

    Refactor Prof page component and update UI components

--- src/app/(Main)/Prof/page.tsx
+++ src/app/(Main)/Prof/page.tsx
@@ -1,97 +1,75 @@
 "use client";
-import { Prof } from "@/app/model";
-import { DataTable } from "@/components/DataTable";
+
 import { Button } from "@/components/ui/button";
-import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown, Pencil, Trash } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { getTeacher } from "@/server/Teacher";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import { FileDown } from "lucide-react";
+import { useEffect, useState } from "react";
 
-const profs: Prof[] = [
-	{
-		Abr_Prof: "CYP",
-		Nom_Prof: "Mr Cyprien",
-	},
-	{
-		Abr_Prof: "RAL",
-		Nom_Prof: "Mr Ralaivao",
-	},
-	{
-		Abr_Prof: "LEA",
-		Nom_Prof: "Mme Léa",
-	},
-	{
-		Abr_Prof: "HAJ",
-		Nom_Prof: "Mr Haja",
-	},
-	{
-		Abr_Prof: "HJS",
-		Nom_Prof: "Mr Hajarisena",
-	},
-	{
-		Abr_Prof: "VOL",
-		Nom_Prof: "Mme Volatiana",
-	},
-	{
-		Abr_Prof: "THM",
-		Nom_Prof: "Mr Thomas",
-	},
-];
+export default function Home() {
+	const [teachers, setTeachers] = useState<Teacher[]>([]);
 
-export default function page() {
-	const columns: ColumnDef<Prof>[] = [
-		{
-			accessorKey: "Abr_Prof",
-			header: "Abréviation de l'enseignant",
-		},
-		{
-			accessorKey: "Nom_Prof",
-			header: ({ column }) => {
-				return (
-					<Button
-						variant="ghost"
-						onClick={() =>
-							column.toggleSorting(column.getIsSorted() === "asc")
-						}>
-						Nom de l'enseignant
-						<ArrowUpDown className="ml-2 h-4 w-4" />
-					</Button>
-				);
-			},
-		},
-		{
-			id: "actions",
-			header: () => {
-				return (
-					<div className="text-center">
-						<p>Action</p>
-					</div>
-				);
-			},
-			cell: ({ row }) => {
-				const prof: Prof = row.original;
-				return (
-					<div className="space-x-3 text-center">
-						<Button className="bg-[#004085]" onClick={() => {}}>
-							<Pencil />
-						</Button>
-						<Button variant="destructive" onClick={() => {}}>
-							<Trash />
-						</Button>
-					</div>
-				);
-			},
-		},
-	];
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = await getTeacher();
+			setTeachers(data);
+		};
+		fetchData();
+	}, []);
+
+	const handleExportPDF = () => {
+		const doc = new jsPDF();
+		autoTable(doc, {
+			head: [["Nom", "Abréviation"]],
+			body: teachers.map((teacher) => [teacher.name, teacher.Abr_Teacher]),
+		});
+		doc.save("liste_des_salles.pdf");
+	};
+
 	return (
-		<div>
-			<DataTable
-				columns={columns}
-				columnsRech="Nom_Prof"
-				placeholderInput="Filter Nom..."
-				data={profs}>
-				<Button className="bg-[#004085]" onClick={() => {}}>
-					Ajouter
-				</Button>
-			</DataTable>
+		<div className="container mx-auto p-4 bg-gray-50 min-h-screen">
+			<Card className="mb-8">
+				<CardHeader className="flex flex-row items-center justify-between">
+					<CardTitle className="text-3xl font-bold text-gray-800">
+						Liste des Enseignants
+					</CardTitle>
+					<div className="flex space-x-2">
+						<Button variant="outline" onClick={handleExportPDF}>
+							<FileDown className="mr-2 h-4 w-4" /> Exporter en PDF
+						</Button>
+					</div>
+				</CardHeader>
+				<CardContent>
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead>ID</TableHead>
+								<TableHead>Nom</TableHead>
+								<TableHead>Abréviation</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{teachers.map((teacher, index) => (
+								<TableRow key={index}>
+									<TableCell>{index + 1}</TableCell>
+									<TableCell>{teacher.name}</TableCell>
+									<TableCell>{teacher.Abr_Teacher}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</CardContent>
+			</Card>
 		</div>
 	);
 }
