8f2aa7d3ec637f5b8ef344cc723b81cd9cb03e06
Ryan <ryanlai2486@gmail.com>
2025-02-15 18:50:44 +0300

    Refactor Teacher.ts to remove console.log statement

--- src/server/Teacher.ts
+++ src/server/Teacher.ts
@@ -2,6 +2,6 @@
 
 export async function getTeacher(): Promise<Teacher[]> {
 	const data: Teacher[] = require("./data/Teacher.json");
-	console.log(data);
+
 	return data.sort((a, b) => a.name.localeCompare(b.name));
 }
