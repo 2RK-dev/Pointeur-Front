e793c6102ae26451f9f900846b09e03952e18f08
Ryan <ryanlai2486@gmail.com>
2025-02-16 14:22:34 +0300

    Refactor edt_utils.ts to optimize time-related functions and add documentation

--- src/lib/edt_utils.ts
+++ src/lib/edt_utils.ts
@@ -1,3 +1,5 @@
+import { getWeekDateRange } from "./common/dateUtils";
+
 export interface hourly {
 	edt_id: number;
 	level: string;
@@ -9,23 +11,9 @@ export interface hourly {
 	date: string;
 }
 
-export interface DayOption {
-	date: string;
-	label: string;
-}
-
 export const CURRENT_YEAR = new Date().getFullYear();
 
 export const BASE_SLOT_HEIGHT = 90;
-export const days: string[] = [
-	"Dimanche",
-	"Lundi",
-	"Mardi",
-	"Mercredi",
-	"Jeudi",
-	"Vendredi",
-	"Samedi",
-];
 
 export const hours = [
 	"7:00",
@@ -122,67 +110,6 @@ export function calculateWidth(startTime: string, endTime: string): number {
 }
 
 /**
- * Returns the current week number of the year.
- * @param year
- * @returns {number}
- * @example getCurrentWeekNumber() => 6
- * @see https://weeknumber.net/how-to/determine-week-numbers
- */
-export function getCurrentWeekNumber(year: number): number {
-	const today = new Date();
-	const firstDayOfYear = new Date(year, 0, 1);
-	const pastDaysOfYear =
-		(today.getTime() - firstDayOfYear.getTime()) / 86400000;
-	return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
-}
-
-/**
- * Returns the start and end dates of a week based on its number and year.
- * @param weekNumber
- * @param year
- * @returns {start: string, end: string}
- * @example getWeekDateRange(1, 2025) => {start: "2024-12-30", end: "2025-01-05"}
- */
-export function getWeekDateRange(
-	weekNumber: number,
-	year: number
-): { start: string; end: string } {
-	const firstDayOfYear = new Date(year, 0, 1);
-	const daysOffset = (weekNumber - 1) * 7;
-	const weekStart = new Date(
-		firstDayOfYear.getTime() + daysOffset * 24 * 60 * 60 * 1000
-	);
-
-	const dayOfWeek = weekStart.getDay(); // 0 (dimanche) à 6 (samedi)
-	const startDate = new Date(weekStart);
-	startDate.setDate(
-		weekStart.getDate() - (dayOfWeek === 0 ? 6 : dayOfWeek - 1)
-	);
-
-	const endDate = new Date(startDate);
-	endDate.setDate(startDate.getDate() + 6); // Fin de semaine (dimanche)
-
-	return {
-		start: startDate.toISOString().split("T")[0], // Format YYYY-MM-DD
-		end: endDate.toISOString().split("T")[0],
-	};
-}
-
-/**
- * Returns the week number of a given date.
- * @param dateString
- * @returns {number}
- * @example getWeekNumber("2025-02-03") => 6
- * @see https://weeknumber.net/how-to/determine-week-numbers
- * */
-export function getWeekNumber(dateString: string): number {
-	const date = new Date(dateString);
-	const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-	const pastDaysOfYear = (date.getTime() - firstDayOfYear.getTime()) / 86400000;
-	return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
-}
-
-/**
  * Returns an array of week options for the current year.
  * @param year
  * @returns {value: string, label: string}[]
@@ -202,65 +129,6 @@ export function getWeekOptions(
 		weekOptions.push({ value: i.toString(), label });
 	}
 	return weekOptions;
-}
-
-/**
- * Returns the day number of a given date.
- * @param dateString
- * @returns {number}
- * @example getDayNumber("2025-02-03") => 1
- * */
-export function getDayNumber(dateString: string): number {
-	const date = new Date(dateString);
-	return date.getDay();
-}
-
-/**
- * get date by week and day number
- * @param year
- * @param week
- * @param day
- * @returns {string}
- * @example getDateByWeekAndDay(2025, 6, 1) => "2025-02-03"
- * */
-export function getDateByWeekAndDay(
-	year: number,
-	week: number,
-	day: number
-): string {
-	const firstDayOfYear = new Date(year, 0, 1);
-	const daysOffset = (week - 1) * 7;
-	const weekStart = new Date(
-		firstDayOfYear.getTime() + daysOffset * 24 * 60 * 60 * 1000
-	);
-
-	const dayOfWeek = weekStart.getDay(); // 0 (dimanche) à 6 (samedi)
-	const startDate = new Date(weekStart);
-	startDate.setDate(
-		weekStart.getDate() - (dayOfWeek === 0 ? 6 : dayOfWeek - 1)
-	);
-
-	const date = new Date(startDate);
-	date.setDate(startDate.getDate() + day);
-	return date.toISOString().split("T")[0];
-}
-
-/**
- * get day options by week number
- * @param {number}weekNumber
- * @param {number}year
- * @returns {date: string, label: string}[]
- * @example getDayOptions(1,2025) => [{date: "2024-12-30", label: "Lundi"}, ...]
- */
-export function getDayOptions(weekNumber: number, year: number): DayOption[] {
-	const dayOptions = [];
-	for (let i = 1; i < 7; i++) {
-		const date = getDateByWeekAndDay(year, weekNumber, i);
-		const label = days[i];
-		dayOptions.push({ date, label });
-	}
-
-	return dayOptions;
 }
 
 /**
@@ -376,4 +244,28 @@ export function calculateRowAssignments(horaires: hourly[]) {
 	});
 
 	return { normalRows: rows };
+}
+
+/**
+ * Transpose the hourlies for a week to a another week, duplicating the data.
+ * @param hourlies
+ * @param numberOfWeeks
+ * @param lastIdNumber
+ * @returns {hourly[]}
+ * @example transposeHourlies([{edt_id: 1, date: "2025-02-03"}, {edt_id: 2, date: "2025-02-04"}], 1, 2) => [{edt_id: 3, date: "2025-02-10"}, {edt_id: 4, date: "2025-02-11"}]
+ */
+export function transposeHourlies(
+	hourlies: hourly[],
+	numberOfWeeks: number,
+	lastIdNumber: number
+): hourly[] {
+	return hourlies.map((horaire, index) => {
+		const date = new Date(horaire.date);
+		date.setDate(date.getDate() + numberOfWeeks * 7);
+		return {
+			...horaire,
+			edt_id: lastIdNumber + index + 1,
+			date: date.toISOString().split("T")[0],
+		};
+	});
 }
