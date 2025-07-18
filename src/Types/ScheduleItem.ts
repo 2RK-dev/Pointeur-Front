ca3d0d0293576e930d431b44337a7e448410df99
Ryan <ryanlai2486@gmail.com>
2025-07-18 14:47:40 +0300

    Refactor: Enhance ScheduleItem schema with preprocessing for date handling and add ScheduleItemPost schema

--- src/Types/ScheduleItem.ts
+++ src/Types/ScheduleItem.ts
@@ -6,12 +6,23 @@ import {GroupSchema} from "@/Types/Group";
 
 export const ScheduleItemSchema = z.object({
   id: z.number(),
-  startTime: z.date(),
-  endTime: z.date(),
+  startTime: z.preprocess((arg) => new Date(arg as string), z.date()),
+  endTime: z.preprocess((arg) => new Date(arg as string), z.date()),
   Teacher: TeacherSchema,
   TeachingUnit: TeachingUnitSchema,
   Room: RoomSchema,
   Groups: z.array(GroupSchema)
 });
 
+export const ScheduleItemPostSchema = z.object({
+    startTime: z.preprocess((arg) => (arg as Date).toISOString(), z.string()),
+    endTime: z.preprocess((arg) => (arg as Date).toISOString(), z.string()),
+    TeacherId: z.number(),
+    TeachingUnitID: z.number(),
+    RoomId: z.number(),
+    GroupIds: z.array(z.string())
+})
+
+
 export type ScheduleItem = z.infer<typeof ScheduleItemSchema>;
+export type ScheduleItemPost = z.infer<typeof ScheduleItemPostSchema>;
