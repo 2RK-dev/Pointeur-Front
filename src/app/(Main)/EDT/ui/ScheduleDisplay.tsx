b5d06dd25e81e24de740aa8f4c134730cc7a83c2
Ryan <ryanlai2486@gmail.com>
2025-07-20 09:56:46 +0300

    Refactor: Simplify rendering logic in ScheduleDisplay component

--- src/app/(Main)/EDT/ui/ScheduleDisplay.tsx
+++ src/app/(Main)/EDT/ui/ScheduleDisplay.tsx
@@ -12,36 +12,31 @@ export default function ScheduleDisplay({jourIndex}: ScheduleDisplayProps) {
         (item) => item.startTime.getDay() === jourIndex
     );
 
-
     const rows = calculateRowAssignments(joursHoraires);
-
-    let lastEnd : Date = new Date();
 
     return (
         <div className="flex flex-col w-full gap-2">
-            {rows.map((row, rowIndex) => (
-                <div
-                    key={rowIndex}
-                    className="relative w-full flex gap-1 min-h-[60px]"
-                >
-
-                    {row.map((scheduleItem, index) => {
-                        let gapMin;
-                        if(index != 0) gapMin = (scheduleItem.startTime.getTime() - lastEnd.getTime()) / 60000;
-                        else {
-                            gapMin = (scheduleItem.startTime.getTime() - new Date(scheduleItem.startTime).setHours(7, 0, 0, 0)) / 60000;
-                        }
-                        lastEnd = scheduleItem.endTime;
-                        return (
-                            <ScheduleItemCard
-                                key={index}
-                                scheduleItem={scheduleItem}
-                                left={getWidthPercentageFor(gapMin)}
-                            />
-                        );
-                    })}
-                </div>
-            ))}
+            {rows.map((row, rowIndex) => {
+                    let lastEnd: Date = new Date();
+                    return (<div key={rowIndex} className="relative w-full flex gap-1 min-h-[60px]">
+                        {row.map((scheduleItem, index) => {
+                            let gapMin;
+                            if (index !== 0) gapMin = (scheduleItem.startTime.getTime() - lastEnd.getTime()) / 60000;
+                            else {
+                                gapMin = (scheduleItem.startTime.getTime() - new Date(scheduleItem.startTime).setHours(7, 0, 0, 0)) / 60000;
+                            }
+                            lastEnd = scheduleItem.endTime;
+                            return (
+                                <ScheduleItemCard
+                                    key={index}
+                                    scheduleItem={scheduleItem}
+                                    left={getWidthPercentageFor(gapMin)}
+                                />
+                            );
+                        })}
+                    </div>)
+                }
+            )}
         </div>
     );
 }
