91e3a7748aa5e00fdf7dacc2d15658eea6941894
Ryan <ryanlai2486@gmail.com>
2025-09-10 17:21:51 +0300

    Delete server old field

--- src/server_old/Teacher.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-export async function getTeacher(): Promise<Teacher[]> {
-	const data: Teacher[] = require("./data/Teacher.json");
-
-	return data.sort((a, b) => a.name.localeCompare(b.name));
-}
