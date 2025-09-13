4a1dbe2570b3f9b34f64f0be0a9b659e8cac9830
rivon0507 <flavienrivon@gmail.com>
2025-09-13 17:27:00 +0300

    Extract base room schema

--- src/api/schemas/room.ts
+++ src/api/schemas/room.ts
@@ -1,20 +1,13 @@
 import z from "zod";
 
-export const RoomSchema = z.object({
+const BaseRoomSchema = z.object({
+    name: z.string(),
+    abbreviation: z.string(),
+    size: z.number(),
+});
+
+export const RoomSchema = BaseRoomSchema.extend({
     id: z.number(),
-    name: z.string(),
-    abbreviation: z.string(),
-    size: z.number(),
 });
-
-export const CreateRoomSchema = z.object({
-    name: z.string(),
-    abbreviation: z.string(),
-    size: z.number(),
-});
-
-export const UpdateRoomSchema = z.object({
-    name: z.string(),
-    abbreviation: z.string(),
-    size: z.number()
-});
+export const CreateRoomSchema = BaseRoomSchema;
+export const UpdateRoomSchema = BaseRoomSchema;
