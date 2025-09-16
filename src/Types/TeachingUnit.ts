5232257c7c79eb26e74aee030625169cbcfde0fc
Ryan <ryanlai2486@gmail.com>
2025-09-16 11:57:51 +0300

    refactor: Allow associatedLevels to be nullable in TeachingUnit model

--- src/Types/TeachingUnit.ts
+++ src/Types/TeachingUnit.ts
@@ -4,7 +4,7 @@ export const TeachingUnitSchema = z.object({
     id: z.number(),
     name: z.string(),
     abr: z.string(),
-    associatedLevels: z.number()
+    associatedLevels: z.number().nullable()
 });
 
 export const TeachingUnitPostSchema = z.object({
