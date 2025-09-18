b434c6f693347a5eb0354a2d5d6c8fe669b0b05a
rivon0507 <flavienrivon@gmail.com>
2025-09-18 16:15:39 +0300

    Implement teacher Create Update Delete Service

--- src/api/http/teacher.ts
+++ src/api/http/teacher.ts
@@ -1,8 +1,22 @@
-import { ITeacher } from "@/api/types";
+import { ICreateTeacher, ITeacher, IUpdateTeacher } from "@/api/types";
 import { http } from "@/api/http/axios";
 import { DTO } from "@/api/schemas";
 
 export async function fetchTeachers (): Promise<ITeacher[]> {
     const {data: responseData} = await http.pub.get("/teachers");
     return DTO.TeacherSchema.array().parse(responseData);
+}
+
+export async function createTeacher (data: ICreateTeacher): Promise<ITeacher> {
+    const {data: responseData} = await http.pub.post("/teachers", data);
+    return DTO.TeacherSchema.parse(responseData);
+}
+
+export async function updateTeacher (teacherId: number, data: IUpdateTeacher): Promise<ITeacher> {
+    const {data: responseData} = await http.pub.put(`/teachers/${teacherId}`, data);
+    return DTO.TeacherSchema.parse(responseData);
+}
+
+export async function deleteTeacher (teacherId: number) {
+    await http.pub.delete(`/teachers/${teacherId}`);
 }
