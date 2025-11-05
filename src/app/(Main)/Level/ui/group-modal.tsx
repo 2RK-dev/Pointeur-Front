40b17d3050b1fca9776fc5bef01e85328e5c5fc5
Ryan <ryanlai2486@gmail.com>
2025-11-06 00:33:59 +0300

    fix: fix the resolver from group-modal.tsx

--- src/app/(Main)/Level/ui/group-modal.tsx
+++ src/app/(Main)/Level/ui/group-modal.tsx
@@ -7,7 +7,7 @@ import {Button} from "@/components/ui/button"
 import {Input} from "@/components/ui/input"
 import {Label} from "@/components/ui/label"
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select"
-import {GroupDTO, GroupDTOSchema, GroupPost} from "@/Types/GroupDTO";
+import {GroupDTO, GroupDTOSchema, GroupPost, GroupPostDTOSchema} from "@/Types/GroupDTO";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Form, FormField} from "@/components/ui/form";
@@ -34,7 +34,7 @@ export function GroupModal({open, onOpenChange, selectGroup, handleUpdateGroup, 
     }
 
     const form = useForm<GroupPost>({
-        resolver: zodResolver(GroupDTOSchema),
+        resolver: zodResolver(GroupPostDTOSchema),
         defaultValues: defaultValue
     })
 
